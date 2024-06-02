11a3ef0e90958ec205fcebdb89cf7813a54e0958
JGManigo <justin.manigo@thecodebox.net>
2024-06-02 13:39:27 +0800

    change p to h1 tag

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <p>Lorem Ipsum</p>
+      <h1>Lorem Ipsum</h1>
     </>
   )
 }
