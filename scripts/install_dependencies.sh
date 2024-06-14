cdaa4a7bd0e13c0c08283033189e21ad691c0079
JGManigo <Justin.manigo@thecodebox.net>
2024-06-15 04:05:43 +0800

    [development] rebase standard folder structrure

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Update the package list
 sudo apt-get update -y
 
@@ -8,6 +7,12 @@ sudo apt-get install -y nodejs npm
 
 # Install PM2 globally
 sudo npm install -g pm2
+
+# Navigate to the app directory
+cd /var/www/html
+
+# Install project dependencies
+npm install
 
 # Install Nginx
 sudo apt-get install -y nginx
