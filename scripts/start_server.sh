97a271b813f6d020a53a363c419943568f6062fa
jmanigo15 <138423278+jmanigo15@users.noreply.github.com>
2024-06-19 21:21:52 +0800

    [development] test

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,10 +5,17 @@ cd /var/www/html/LOTTO
 # Build the React Vite app
 npm run build
 
+# Start the app using PM2
+pm2 start npm --name "vite-app" -- start
 
+# Ensure PM2 starts on boot
+pm2 startup systemd
+sudo env PATH=$PATH:/usr/bin pm2 startup systemd -u $USER --hp /home/$USER
+pm2 save
 
 # Enable and start Nginx
 sudo systemctl enable nginx
 sudo systemctl start nginx
 
-
+# Restart all PM2 managed apps
+pm2 restart all
