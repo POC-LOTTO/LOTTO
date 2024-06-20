567aa49121f9a67473ec2430523d58a36228e19d
jmanigo15 <138423278+jmanigo15@users.noreply.github.com>
2024-06-20 14:34:31 +0800

    213

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Check if the server is running by sending a request to localhost
-HTTP_STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" http://52.77.96.4/)
+# HTTP_STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" http://52.77.96.4/)
 
-echo "HTTP Status: $HTTP_STATUS"
+# echo "HTTP Status: $HTTP_STATUS"
 
-if [ "$HTTP_STATUS" -eq 200 ]; then
-  echo "Validation successful"
-  exit 0
-else
-  echo "Validation failed"
-  exit 1
-fi
+# if [ "$HTTP_STATUS" -eq 200 ]; then
+#   echo "Validation successful"
+#   exit 0
+# else
+#   echo "Validation failed"
+#   exit 1
+# fi
